253ba155666e77e0e1dec16092f159daa8a57f67
Loïc Touzard <ltouzard@degustandco.com>
2015-06-17 14:55:39 +0200

    [jacquesIdea] Ajax d'envoi de commentaire

--- ajax/views.py
+++ ajax/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
@@ -8,13 +8,19 @@ from jacquesIdea.models import Idee, Commentaire
 
 
 def jacquesIdeaEnregistrerCommentaire(request):
+    #recuperation des parametre
     idee_id = request.POST['idee_id']
     commentaire_text = request.POST['com']
     idee = get_object_or_404(Idee, pk=idee_id)
+
+    # Creation du commentaire
     com = Commentaire()
     com.idee = idee
     com.auteur = request.user
     com.commentaire_text = commentaire_text
     com.pub_date = timezone.now()
     com.save()
-    return HttpResponse()
+
+    #generation du template
+    context = {'commentaire': com}
+    return render(request, 'ajax/rightComment.html', context)
