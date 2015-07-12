925e34a01a01cc24f5d5c928ec31958a1628c665
Loïc Touzard <ltouzard@degustandco.com>
2015-07-12 21:04:53 +0200

    [userManager] implémentation du signal post_save sur User pour creer un userProfil par défaut

--- userManager/models.py
+++ userManager/models.py
@@ -1,9 +1,10 @@
 from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
 
 # Create your models here.
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
@@ -12,3 +13,10 @@ class UserProfile(models.Model):
     telephone = models.CharField(max_length=10, validators=[phone_regex], blank=True, null=True)  # validators should be a list
     avatar = models.ImageField(upload_to="uploads", blank=True, null=True)
     # TODO null=True et image par défaut
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+       profile, created = UserProfile.objects.get_or_create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
