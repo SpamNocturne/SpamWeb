51adbf03da7c583e294edd9088cfd0942d6b4aa5
David Wobrock <david.wobrock@gmail.com>
2015-07-19 13:17:34 +0200

    Avancemant spamalyzer (non fonctionnel, ni fini)

--- SpamAlyzer/views.py
+++ SpamAlyzer/views.py
@@ -1,35 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from SpamAlyzer.forms import FichierSoumisForm
 from SpamAlyzer import analyzer
 
-from .forms import FichierSoumisForm
-
-import SpamAlyzer.analyzer
+from lxml import etree
 
 @login_required
 def index(request):
     # If POST, then an upload has been made
-    context = {}
+    context = {"error_message":"oui"}
     if request.method == "POST":
-        # Get file
         form = FichierSoumisForm(request.POST, request.FILES)
         if form.is_valid():
-            depotFichier = form.save(commit=False)
-            depotFichier.auteur = request.user
+            fichier = form.save(commit=False)
+            fichier.auteur = request.user
 
-
-            if analyzer.has_correct_xml_format(depotFichier.fichier):
-                depotFichier.save()
-                context['conversation'] = "FONCTIONER"
-            else:
-                context['conversation'] = "FORMAT PAS BON"
+            try:
+                anal = analyzer.Analyzer(fichier.fichier)
+                msg = anal.get_spam_messages() # TODO, ca va etre plus compliqué que ca
+            except etree.DocumentInvalid:
+                context["error_message"] = "Oh non! Ton fichier est tout naze. " \
+                                           "Python me dit dans l'oreillette DocumentInvalid. " \
+                                           "C'est triste. Et franchement, tu me déçois. Je te croyais mieux que ça."
+                return render_to_response('SpamAlyzer/message.html', context)
         else:
-            context['conversation'] = "PAS FONCT"
+            context["error_message"] = "Oh non! Ton fichier est tout naze. " \
+                                       "Python me dit dans l'oreillette que le formulaire n'est pas valide. " \
+                                       "C'est triste. Et franchement, tu me déçois. Je te croyais mieux que ça."
+            return render_to_response('SpamAlyzer/index.html', context)
+    # No POST made, normal
     else:
+        context["conversation"] = None
         form = FichierSoumisForm()
-        context['conversation'] =  None
-    context["form"] = form
 
+    context["form"] = form
     return render(request, 'SpamAlyzer/index.html', context)
