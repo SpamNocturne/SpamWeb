68c4e575459be0e749799985bc8221b17b8f3d37
Loïc Touzard <loic.touzard@gmail.com>
2015-07-20 23:07:00 +0200

    [Global] Ajout des notifications sur la home + texte d'acceuil pour la home

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
+from .models import Log
 
 # Create your views here.
+
+
 @login_required
 def index(request):
-    template_name = 'home/index.html'
+    logs = Log.objects.order_by('-date')
     context = {
+        'logs': logs
     }
-    return render(request, template_name, context)
+    return render(request, 'home/index.html', context)
