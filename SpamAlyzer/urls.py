67787d9e1fb3ba94f7154905af9b19e64e9a27c2
David Wobrock <david.wobrock@gmail.com>
2015-07-21 00:12:47 +0200

    historique des depots + mini modif algo analyse spam

--- SpamAlyzer/urls.py
+++ SpamAlyzer/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^conversation/spam/web/(?P<num_page>[0-9]+)', views.conversation, name="conversation")
+    url(r'^conversation/spam/web/(?P<num_page>[0-9]+)', views.conversation, name="conversation"),
+    url(r'^historique/depot', views.historique, name="histoDepot")
 ]
