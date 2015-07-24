928e25a6c6c29a2f25ca3bd6798d7613c0483931
Gazolike <tiobilouteandra@hotmail.fr>
2015-07-24 23:05:38 +0200

    debut css spamConso et bieres

--- spamConso/urls.py
+++ spamConso/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index', views.index, name='index'),
-    url(r'^get_conso', views.get_conso, name='get_conso'),
+    url(r'^GBienBuGBienMangé', views.index, name='index'),
     url(r'^add_conso', views.add_conso, name = 'add_conso'),
+    url(r'^bieres', views.beer_view, name = 'beer_view'),
 
 ]
